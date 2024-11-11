2bb4d1c53442610fb00368cb9450ddf2054cf1cd
Willfoss <118803774+Willfoss@users.noreply.github.com>
2024-11-11 15:34:08 +0000

    added routing

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
 
 function App() {
   return (
     <main>
-      <Login />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
     </main>
   );
 }
