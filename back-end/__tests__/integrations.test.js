30e7a84ef6cecff8995f75795d5beddd0a4a5a0c
Willfoss <118803774+Willfoss@users.noreply.github.com>
2024-11-15 09:57:12 +0000

    added extra property in post 201 test

--- back-end/__tests__/integrations.test.js
+++ back-end/__tests__/integrations.test.js
@@ -23,6 +23,7 @@ describe("back end testing", () => {
             name: "Jeff B",
             email: "jeffyB@amazon.com",
             password: expect.any(String),
+            picture: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
           });
         });
     });
